72cc1d7664694960bdf17474b8a924142797ccf2
Pratham Gupta <prathamgupta.wk@gmail.com>
2026-01-21 14:56:29 +0530

    message frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,30 @@
+import { useRef, useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 function App() {
+    const [message, setMessage] = useState([]);
+    const messageUser = useRef<HTMLInputElement>(null);
+    function handleEnter(e: React.KeyboardEvent) {
+        if (e.code == 'Enter') {
+            if (messageUser.current?.value) {
+                messageUser.current.value = '';
+            }
+        }
+    }
 
     return (
         <>
             <Navbar />
             <div className="parent">
+                <div className="user">hello world</div>
+                <div className="user">hello world</div>
+                <div className="user">hello world</div>
                 <div className="inbox">
                     <input
                         type="text"
                         className="message"
+                        ref={messageUser}
+                        onKeyDown={handleEnter}
                     />
                 </div>
             </div>
