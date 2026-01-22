86953b9a4af32bb8b052e2839ee24acabda1a73e
Pratham Gupta <prathamgupta.wk@gmail.com>
2026-01-22 12:24:39 +0530

    it works

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,53 @@
-import { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import api from './axios/api';
 function App() {
-    const [message, setMessage] = useState([]);
-    const messageUser = useRef<HTMLInputElement>(null);
+    type messageType = {
+        user: string;
+        comp: string;
+    };
+
+    const [message, setMessage] = useState<messageType[]>([]);
+    const messageAreaR = useRef<HTMLDivElement>(null);
+
     function handleEnter(e: React.KeyboardEvent) {
         if (e.code == 'Enter') {
-            if (messageUser.current?.value) {
-                messageUser.current.value = '';
-            }
+            const val = (e.target as HTMLInputElement).value;
+            setMessage((prev) => [...prev, { user: val, comp: '' }]);
+            (e.target as HTMLInputElement).value = '';
         }
     }
+
+    useEffect(() => {
+        if (messageAreaR.current) {
+            messageAreaR.current.scrollTop = messageAreaR.current?.scrollHeight;
+        }
+        if(message[message.length - 1].user){
+            api.post('/ask')
+        }
+    }, [message]);
 
     return (
         <>
             <Navbar />
             <div className="parent">
-                <div className="user">hello world</div>
-                <div className="user">hello world</div>
-                <div className="user">hello world</div>
+                <div className="messageArea" ref={messageAreaR}>
+                    {message.length == 0
+                        ? null
+                        : message.map((e) => {
+                              if (e.user) {
+                                  return <div className="userM">{e.user}</div>;
+                              }
+                              if (e.comp) {
+                                  return <div className="compM">{e.comp}</div>;
+                              }
+                          })}
+                </div>
                 <div className="inbox">
                     <input
                         type="text"
                         className="message"
-                        ref={messageUser}
                         onKeyDown={handleEnter}
                     />
                 </div>
